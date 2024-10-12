186c14257f29fbeb79a23c872b3a9a903dd16f02
Mahmoud Ahmed <73077353+mahmoudahme@users.noreply.github.com>
2024-10-12 07:11:14 +0300

    Delete Utils directory

--- Utils/apiError.js
+++
@@ -1,8 +0,0 @@
-export class ApiError extends Error {
-  constructor(message, statusCode) {
-    super(message);
-    this.statusCode = statusCode;
-    this.status = `${statusCode}`.startsWith(4) ? "fail" : "error";
-    this.isOperational = true;
-  }
-}
